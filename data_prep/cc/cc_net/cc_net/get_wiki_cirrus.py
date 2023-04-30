42c8859ae61c0fa3b37df5010d01dd6d70854155
Your Name <you@example.com>
2023-04-30 11:52:13 +0000

    fix: make cc perplexity compatible with thai

--- data_prep/cc/cc_net/cc_net/get_wiki_cirrus.py
+++ data_prep/cc/cc_net/cc_net/get_wiki_cirrus.py
@@ -39,7 +39,7 @@ def opening(file: Path, output: Path = None, n_docs: int = 1_000_000):
         - lang: Language code used to chose the tokenizer
     """
     assert file.exists()
-    return jsonql.run_pipes(
+    jsonql.run_pipes(
         functools.partial(extract_opening_text, n_docs=n_docs),
         file=file,
         output=tmp(output) if output else None,
@@ -58,7 +58,7 @@ def extract_opening_text(source, n_docs: int = 10_000):
         if not text:
             continue
 
-        yield text_normalizer.normalize(text)
+        yield text_normalizer.normalize(text, accent = False)
         i += 1
         if i >= n_docs:
             break
